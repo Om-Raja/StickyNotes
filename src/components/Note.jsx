7ef76000e8c0066397cb18d3c6fbb9ad4c8354a2
Om Raja <omraja451@gmail.com>
2025-09-10 22:54:41 +0530

    add buttons UI for notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,13 +2,32 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function Note() {
+  function capitalize(str){
+    if(!str) return "";
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
   const notes = useSelector((state) => state.notes.notes);
   return (
     <div className="mt-4 p-2">
       {notes.map((note) => (
-        <div key={note._id} className="border mt-2 rounded-md bg-[var(--color-surface)]">
-          <div className="mb-1 font-semibold text-lg">{note.title}</div>
-          <p>{note.content}</p>
+        <div
+          key={note._id}
+          className="border m-3 rounded-md bg-[var(--color-surface)]"
+        >
+          <div className="mt-1">
+          <div className="font-semibold text-lg">{capitalize(note.title)}</div>
+          <span>{note.createdAt}</span>
+          </div>
+          <p className="mt-1 mb-2">{capitalize(note.content)}</p>
+
+          <div className="flex flex-row justify-evenly mt-3 mb-2">
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">View</button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">Edit</button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">Copy</button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">Delete</button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">Share</button>
+          </div>
         </div>
       ))}
     </div>
