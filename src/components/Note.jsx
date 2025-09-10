3192a01760c28df350e4db6d3f6cb0bf5fd49e21
Om Raja <omraja451@gmail.com>
2025-09-10 23:08:56 +0530

    format date on note in human readable format

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { format } from "date-fns";
 
 function Note() {
-  function capitalize(str){
-    if(!str) return "";
+  function capitalize(str) {
+    if (!str) return "";
     return str[0].toUpperCase() + str.slice(1);
   }
 
@@ -13,20 +14,32 @@ function Note() {
       {notes.map((note) => (
         <div
           key={note._id}
-          className="border m-3 rounded-md bg-[var(--color-surface)]"
+          className="border m-3 p-2 rounded-md bg-[var(--color-surface)]"
         >
-          <div className="mt-1">
-          <div className="font-semibold text-lg">{capitalize(note.title)}</div>
-          <span>{note.createdAt}</span>
+          <div className="mt-1 flex flex-row justify-between">
+            <div className="ml-4 font-semibold text-lg">
+              {capitalize(note.title)}
+            </div>
+            <span className="mr-4">{format(note.createdAt, "dd MMM yy, hh:mm a")}</span>
           </div>
           <p className="mt-1 mb-2">{capitalize(note.content)}</p>
 
           <div className="flex flex-row justify-evenly mt-3 mb-2">
-            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">View</button>
-            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">Edit</button>
-            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">Copy</button>
-            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">Delete</button>
-            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">Share</button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">
+              View
+            </button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">
+              Edit
+            </button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">
+              Copy
+            </button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">
+              Delete
+            </button>
+            <button className="bg-[var(--color-primary)] text-[var(--color-textdark)] px-2 py-1.5 rounded-md hover:bg-[var(--color-secondary)] hover:text-[var(--color-primary)] transform hover:scale-110 transition-transform duration-200 ease-in-out">
+              Share
+            </button>
           </div>
         </div>
       ))}
