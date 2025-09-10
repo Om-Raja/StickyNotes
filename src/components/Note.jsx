c4dfd311f0b6032ba00c6d9b65db080c6239d602
Om Raja <omraja451@gmail.com>
2025-09-10 22:23:28 +0530

    render UI of all notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 function Note() {
+  const notes = useSelector((state) => state.notes.notes);
   return (
-    <div>Note</div>
-  )
+    <div className="mt-4 p-2">
+      {notes.map((note) => (
+        <div key={note._id} className="border mt-2 rounded-md bg-[var(--color-surface)]">
+          <div className="mb-1 font-semibold text-lg">{note.title}</div>
+          <p>{note.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Note
+export default Note;
